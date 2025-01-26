0620ab5d54efaa19ba6175bc62913eb0413d5392
Mudigonda45 <20315A1212@sreenidhi.edu.in>
2025-01-26 13:11:39 +0530

    changes

---
+++ browser/assets/partition.js
@@ -0,0 +1,7 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     // Example using particles.js library
+//     particlesJS.load('hero-background', 'particles.json', function () {
+//       console.log('Particles.js config loaded');
+//     });
+//   });
+  
